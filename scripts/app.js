09cdb4fc5f44befcfb7493706f9403222128c600
George Wiggin <george@sequest.io>
2022-07-01 15:58:57 +0100

    basic grid set up

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,28 @@
-console.log('Connected')
-
-
+console.log('connected')
 
 function init() {
 
   // * Player set up
-  
+
 
   // * Create Grid
+  const grid = document.querySelector('.grid')
 
+  const width = 18
+  const cellCount = width * width
+  const cells = []
+
+  function createGrid(){
+    for (let i = 0; i < cellCount; i++){
+      const cell = document.createElement('div')
+      cell.innerText = i
+      cell.id = i
+      grid.appendChild(cell)
+      cells.push(cell)
+    }
+    console.log(cells)
+  }
+  createGrid()
 
 
 
