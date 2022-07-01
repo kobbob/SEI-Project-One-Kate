dc0f349519620768a9b3dac7de22dcb6c6828f80
George Wiggin <george@sequest.io>
2022-07-01 19:32:19 +0100

    css grid still in progress

--- scripts/app.js
+++ scripts/app.js
@@ -2,16 +2,39 @@ console.log('connected')
 
 function init() {
 
-  // * Player set up
+  // * Element set up -------------------------
+  // const marioClass = 'mario'
+  // const marioStart =
+
+  // const bowserClass = 'bowser'
+  // const waluigiClass = 'waluigi'
+  // const kingbooClass = 'kingboo'
+
+  const wallClass = 'wall'
+  const removeCellClass = 'removeCell'
+  const penClass = 'pen'
+  const leftborderClass = 'leftborder'
+  const rightborderClass = 'rightborder'
+  const topborderClass = 'topborder'
+  // const starClass = 'star'
 
 
-  // * Create Grid
+  // * GRID CONFIGURATION >>>>>>>>>>>
+
   const grid = document.querySelector('.grid')
 
   const width = 18
   const cellCount = width * width
   const cells = []
+  const wallCells = []
+  const removeCells = []
+  const penCells = []
+  const leftborderCells = []
+  const rightborderCells = []
+  const topborderCells = []
 
+
+  // * Create Grid ----------------------------
   function createGrid(){
     for (let i = 0; i < cellCount; i++){
       const cell = document.createElement('div')
@@ -25,6 +48,128 @@ function init() {
   createGrid()
 
 
+  // * Create Walls ---------------------------
+  function addWallCells() {
+    for (let i = 0; i < cells.length; i++) {
+      if ((i === 20 || i === 38 || i === 22 || i === 23 || i === 24 || i === 29 || i === 30 || i === 31 || i === 33
+        || i === 38 || i === 51
+        || i === 58 || i === 59 || i === 60 || i === 62 || i === 63 || i === 65 || i === 66 || i === 67
+        || i === 73 || i === 74 || i === 77 || i === 80 || i === 81 || i === 84 || i === 87 || i === 88
+        || i === 97 || i === 98 || i === 99 || i === 100
+        || i === 112 || i === 113 || i === 120 || i === 121
+        || i === 130 || i === 131 || i === 138 || i === 139
+        || i === 166 || i === 167 || i === 174 || i === 175
+        || i === 184 || i === 185 || i === 192 || i === 193
+        || i === 205 || i === 206 || i === 207 || i === 208
+        || i === 217 || i === 218 || i === 220 || i === 221 || i === 224 || i === 225 || i === 228 || i === 229 || i === 231 || i === 232
+        || i === 236 || i === 249
+        || i === 254 || i === 256 || i === 258 || i === 259 || i === 260 || i === 261 || i === 262 || i === 263 || i === 265 || i === 267
+        || i === 274 || i === 278 || i === 279 || i === 283
+        || i === 289 || i === 290 || i === 291 || i === 292 || i === 293 || i === 294 || i === 296 || i === 297 || i === 299 || i === 300 || i === 301 || i === 302 || i === 303 || i === 304)) {
+        wallCells.push(cells[i])
+        wallCells.forEach(wallCell => {
+          wallCell.classList.add(wallClass)
+        })
+      }
+    }
+  }
+  addWallCells()
+
+
+  // * Remove Cells ---------------------------
+  function removeGridCells() {
+    for (let i = 0; i < cells.length; i++) {
+      if (( i === 8 || i === 9
+        || i === 26 || i === 27
+        || i === 36 || i === 53
+        || i === 108 || i === 109 || i === 110 || i === 123 || i === 124 || i === 125
+        || i === 126 || i === 127 || i === 128 || i === 141 || i === 142 || i === 143
+        || i === 162 || i === 163 || i === 164 || i === 177 || i === 178 || i === 179
+        || i === 180 || i === 181 || i === 182 || i === 195 || i === 196 || i === 197
+        || i === 252 || i === 269)) {
+        removeCells.push(cells[i])
+        removeCells.forEach(removeCell => {
+          removeCell.classList.add(removeCellClass)
+        })
+      }
+    }
+  }
+  removeGridCells()
+
+
+  // * Create Pen ---------------------------
+  function createPen() {
+    for (let i = 0; i < cells.length; i++) {
+      if (( i === 133 || i === 134 || i === 135 || i === 136 || i === 151 || i === 152 || i === 153 || i === 154 || i === 169 || i === 170 || i === 171 || i === 172)) {
+        penCells.push(cells[i])
+        penCells.forEach(penCell => {
+          penCell.classList.add(penClass)
+        })
+      }
+    }
+  }
+  createPen()
+
+
+  // * Create Left Edge Border ---------------------------
+  function createLeftBorder() {
+    for (let i = 0; i < cells.length; i++) {
+      if (( i === 0 || i === 18 || i === 37 || i === 54 || i === 72 || i === 90 || i === 111 || i === 129 || i === 144 || i === 165 || i === 183 || i === 198 || i === 216 || i === 234 || i === 253 || i === 270 || i === 288 || i === 306
+        || i === 73 || i === 217 || i === 289
+        || i === 20 || i === 38 || i === 236 || i === 254
+        || i === 21 || i === 39 || i === 75 || i === 219 || i === 237 || i === 255
+        || i === 22 || i === 58 || i === 112 || i === 130 || i === 166 || i === 184 || i === 220 || i === 256 || i === 274
+        || i === 77 || i === 257 || i === 275
+        || i === 78 || i === 114 || i === 132 || i === 168 || i === 186 || i === 222 || i === 258
+        || i === 97 || i === 133 || i === 151 || i === 169 || i === 205
+        || i === 25 || i === 61 || i === 295
+        || i === 8 || i === 26 || i === 62 || i === 80 || i === 224 || i === 278 || i === 296
+        || i === 29 || i === 65 || i === 299
+        || i === 84 || i === 120 || i === 138 || i === 174 || i === 192 || i === 228
+        || i === 265 || i === 283
+        || i === 33 || i === 51 || i === 87 || i === 123 || i === 141 || i === 177 || i === 195 || i === 231 || i === 249 || i === 267)) {
+        leftborderCells.push(cells[i])
+        leftborderCells.forEach(leftborderCell => {
+          leftborderCell.classList.add(leftborderClass)
+        })
+      }
+    }
+  }
+  createLeftBorder()
+
+
+  // * Create Right Edge Border ---------------------------
+  function createRightBorder() {
+    for (let i = 0; i < cells.length; i++) {
+      if (( i === 9 || i === 27 || i === 63 || i === 81 || i === 225 || i === 279 || i === 297
+        || i === 100 || i === 136 || i === 154 || i === 172 || i === 208 || i === 263
+        || i === 84
+        || i === 31 || i === 67 || i === 121 || i === 139 || i === 175 || i === 193 || i === 229 || i === 265 || i === 283
+        || i === 33 || i === 51 || i === 249 || i === 267
+        || i === 52 || i === 88 || i === 232 || i === 304 || i === 268
+        || i === 17 || i === 35 || i === 71 || i === 89 || i === 107 || i === 161 || i === 215 || i === 233 || i === 251 || i === 287 || i === 305 || i === 323 )) {
+        rightborderCells.push(cells[i])
+        rightborderCells.forEach(rightborderCell => {
+          rightborderCell.classList.add(rightborderClass)
+        })
+      }
+    }
+  }
+  createRightBorder()
+
+  // * Create Top Edge Border ---------------------------
+  function createTopBorder() {
+    for (let i = 0; i < cells.length; i++) {
+      if (( i === 0 || i === 1 || i === 2 || i === 3 || i === 4 || i === 5 || i === 6 || i === 7 || i === 10 || i === 11 || i === 12 || i === 13 || i === 14 || i === 15 || i === 16 || i === 17
+        || i === 20 )) {
+        topborderCells.push(cells[i])
+        topborderCells.forEach(topborderCell => {
+          topborderCell.classList.add(topborderClass)
+        })
+      }
+    }
+  }
+  createTopBorder()
 
 
 }
